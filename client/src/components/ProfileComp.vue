ae3c3011d7ea9f87d9e7d873b4794c0f4b78fd88
Braden Dykstra <Toastie285@gmail.com>
2019-09-13 16:50:40 -0600

    get profile's broke, fix it future me

--- client/src/components/ProfileComp.vue
+++ client/src/components/ProfileComp.vue
@@ -1,17 +1,24 @@
 //FIXME 
 <template>
   <div class="Profile-Comp card col-4">
-    <img :src="profile.img" alt="BIO IMAGE" />
-    <h3>{{profile.username}}</h3>
-    <h5>{{profile.bio}}</h5>
-    <EditProfileModal />
-    <CreateProfileModal />
-    <button class="btn btn-info" data-toggle="modal" data-target="#create-profile-modal">
-      <i class="fas fa-pencil-alt"></i> Create Profile
-    </button>
-    <button class="btn btn-warning" data-toggle="modal" data-target="#edit-profile-modal">
+    <img :src="profile.img" alt="BIO IMAGE" class="card-img-top" />
+    <div class="card-body">
+      <h3 class="card-title">{{profile.username}}</h3>
+      <h5 class="card-text">{{profile.bio}}</h5>
+      <EditProfileModal />
+      <CreateProfileModal />
+      <button
+        class="btn btn-info"
+        data-toggle="modal"
+        data-target="#create-profile-modal"
+        v-if="!profile.username"
+      >
+        <i class="fas fa-pencil-alt"></i> Create Profile
+      </button>
+      <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#edit-profile-modal" v-if="profile.username">
       <i class="fas fa-pencil-alt"></i> Edit Profile
-    </button>
+      </button>-->
+    </div>
   </div>
 </template>
 
@@ -24,13 +31,18 @@ export default {
   data() {
     return {};
   },
+  mounted() {
+    this.getProfile();
+  },
   computed: {
     profile() {
       return this.$store.state.user;
     }
   },
   methods: {
-    // editProfile() {}
+    getProfile() {
+      this.$store.dispatch("getProfile");
+    }
   },
   components: { EditProfileModal, CreateProfileModal }
 };
@@ -38,4 +50,8 @@ export default {
 
 
 <style scoped>
+img {
+  max-height: 200px;
+  max-width: 200px;
+}
 </style>
