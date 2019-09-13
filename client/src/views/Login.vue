977ee2c334b5867dc7c002a4601d2e481e7e2fef
Zach Pfeifer <mr.zman1@yahoo.com>
2019-09-13 15:05:11 -0600

    fixed Login Vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -38,7 +38,7 @@ export default {
   },
   beforeCreate() {
     if (this.$store.state.user._id) {
-      this.$router.push({ name: "home" });
+      this.$router.push({ name: "Profile" });
     }
   },
   methods: {
