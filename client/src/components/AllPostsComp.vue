2a277cdc5ce74891b849e650ec86acedbfb5f6dc
Zach Pfeifer <mr.zman1@yahoo.com>
2019-09-13 11:04:37 -0600

    getposts

---
+++ client/src/components/AllPostsComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="AllPostsComp">
+    <PostComp v-for="post in posts" :postProp="post" :key="post._id" />
+  </div>
+</template>
+
+
+<script>
+import PostComp from "./PostComp";
+export default {
+  name: "AllPostsComp",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.$store.dispatch("getPosts");
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    }
+  },
+  methods: {},
+  components: { PostComp }
+};
+</script>
+
+
+<style scoped>
+</style>
