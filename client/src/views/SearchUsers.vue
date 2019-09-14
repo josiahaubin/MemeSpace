3795b0f39c0f296b3da0d3ec1d5886acd106e7a3
Braden Dykstra <Toastie285@gmail.com>
2019-09-13 20:54:36 -0600

    search page in router

---
+++ client/src/views/SearchUsers.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="searchUsers">
+    <SearchUsersComp />
+  </div>
+</template>
+
+
+<script>
+import SearchUsersComp from "../components/SearchUsersComp";
+export default {
+  name: "searchUsers",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: { SearchUsersComp }
+};
+</script>
+
+
+<style scoped>
+</style>
